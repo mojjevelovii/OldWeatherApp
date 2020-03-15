dc7a6d8955b2776db850dccaf0a2ef9fb3e45f04
Veronika Shumilova <mojjevelovii@gmail.com>
2020-03-15 12:40:44 +0300

    Added two screens according practical task.

---
+++ app/src/main/java/ru/geekbrains/weatherApp/CitySelectionActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.weatherApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CitySelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.city_selection);
+    }
+}
