ac4f74a18e74946c88de9fd02713db4dd9c78e99
Veronika Shumilova <mojjevelovii@gmail.com>
2020-03-21 20:31:31 +0300

    Задание №1.

--- app/src/main/java/ru/geekbrains/weatherApp/CitySelectionActivity.java
+++ app/src/main/java/ru/geekbrains/weatherApp/CitySelectionActivity.java
@@ -2,13 +2,40 @@ package ru.geekbrains.weatherApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 
 public class CitySelectionActivity extends AppCompatActivity {
+    static final String CITY_NAME = "CITY_NAME";
+    private EditText etInputCity;
+    private ImageView ivSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.city_selection);
+        initView();
+    }
+
+    private void initView() {
+        etInputCity = findViewById(R.id.et_city_search);
+        ivSearch = findViewById(R.id.iv_search);
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNewCity();
+            }
+        });
+    }
+
+    private void setNewCity() {
+        Intent intentResult = new Intent();
+        intentResult.putExtra(CITY_NAME, etInputCity.getText().toString());
+        setResult(RESULT_OK, intentResult);
+        finish();
     }
 }
--- app/src/main/java/ru/geekbrains/weatherApp/MainActivity.java
+++ app/src/main/java/ru/geekbrains/weatherApp/MainActivity.java
@@ -2,14 +2,44 @@ package ru.geekbrains.weatherApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int SELECT_CITY_REQUEST_CODE = 12;
+    private TextView tvCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initView();
     }
+
+    private void initView() {
+        tvCity = findViewById(R.id.tv_city);
+        tvCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectCity();
+            }
+        });
+    }
+
+    private void selectCity() {
+        Intent intent = new Intent(this, CitySelectionActivity.class);
+        startActivityForResult(intent, SELECT_CITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_CITY_REQUEST_CODE) {
+            tvCity.setText(data.getStringExtra(CitySelectionActivity.CITY_NAME));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
